3af5a065e1d76b40d9165339d69ca053c8532910
MyLittleChicken <jackbomb51@gmail.com>
2025-03-14 10:24:51 +0900

    feat: 도메인 차단을 위한 객체 구현
    1. BlockDomainLoader: 파일 다운로드, 읽기 등을 수행하여 차단될 도메인 정보를 제공하는 클래스
    2. BlockDomains: 차단이 필요한 도메인에 대한 정보를 제공하는 클래스
    3. application.yml: hosts.txt 파일에 대한 공통 변수 추가

---
+++ src/main/java/community/whatever/onembackendjava/BlockDomains.java
@@ -0,0 +1,41 @@
+package community.whatever.onembackendjava;
+
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.Set;
+
+@Component
+public class BlockDomains {
+    private final Set<String> domains;
+
+    public BlockDomains(final BlockDomainLoader blockDomainLoader) {
+        this.domains = blockDomainLoader.load();
+    }
+
+    public boolean isBlocked(final String authority) {
+        if (authority == null || authority.isBlank()) {
+            return true;
+        }
+
+        return this.domains.contains(authority);
+    }
+
+    public boolean isBlocked(final URI uri) {
+        if (uri == null || uri.getAuthority().isBlank()) {
+            return true;
+        }
+
+        return this.domains.contains(uri.getAuthority());
+    }
+
+    public boolean isBlocked(final URL url) {
+        if (url == null || url.getAuthority().isBlank()) {
+            return true;
+        }
+
+        return this.domains.contains(url.getAuthority());
+    }
+
+}
