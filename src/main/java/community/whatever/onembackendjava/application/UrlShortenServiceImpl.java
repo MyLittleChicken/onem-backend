f9c7631da0165093873d43d75974e4142d2c5a8c
MyLittleChicken <jackbomb51@gmail.com>
2025-03-14 11:33:34 +0900

    feat: BlockDomains 를 통해 block 이 필요한 도메인에 대한 내용을 검증하는 내용 추가

--- src/main/java/community/whatever/onembackendjava/application/UrlShortenServiceImpl.java
+++ src/main/java/community/whatever/onembackendjava/application/UrlShortenServiceImpl.java
@@ -1,21 +1,28 @@
 package community.whatever.onembackendjava.application;
 
+import community.whatever.onembackendjava.BlockDomainException;
+import community.whatever.onembackendjava.BlockDomains;
 import community.whatever.onembackendjava.CustomDuplicateKeyException;
 import community.whatever.onembackendjava.repository.UrlShortenRepository;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
 
 @Service
 public class UrlShortenServiceImpl implements UrlShortenService {
 
     private final UrlShortenRepository urlShortenRepository;
     private final RandomKeyGenerator randomKeyGenerator;
+    private final BlockDomains blockDomains;
 
     public UrlShortenServiceImpl(
             final UrlShortenRepository urlShortenRepository,
-            final RandomKeyGenerator randomKeyGenerator
+            final RandomKeyGenerator randomKeyGenerator,
+            final BlockDomains blockDomains
     ) {
         this.urlShortenRepository = urlShortenRepository;
         this.randomKeyGenerator = randomKeyGenerator;
+        this.blockDomains = blockDomains;
     }
 
     @Override
@@ -26,6 +33,10 @@ public class UrlShortenServiceImpl implements UrlShortenService {
 
     @Override
     public String createShortUrl(final String originUrl) throws CustomDuplicateKeyException {
+        if (blockDomains.isBlocked(URI.create(originUrl))) {
+            throw new BlockDomainException(originUrl);
+        }
+
         String randomKey = randomKeyGenerator.getRandomKey();
 
         if (urlShortenRepository.existsByKey(randomKey)) {
